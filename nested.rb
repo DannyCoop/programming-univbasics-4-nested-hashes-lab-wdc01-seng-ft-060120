d341a7579fcf4140d703d27e1ba3d03cead3024f
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-05-20 02:39:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,12 +84,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    programmer_hash[:yukihiro_matsumoto] = {:known_for}
-    programmer_hash[:yukihiro_matsumoto][:known_for] << "ruby"
-    programmer_hash[:yukihiro_matsumoto] << :languages
-    programmer_hash[:yukihiro_matsumoto][:languages << ["LISP",
-    programmer_hash
-
+    
 end
 
 def changing_alan
